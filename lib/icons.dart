39c901a9822b769d3f7a9bc84715a0aa0360ffde
jchillah <jchillah@gmail.com>
2024-04-17 13:37:53 +0200

    heart changes color on press

--- lib/icons.dart
+++ lib/icons.dart
@@ -1,14 +1,36 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-class MyIconsRow extends StatelessWidget {
+class MyIconsRow extends StatefulWidget {
   const MyIconsRow({super.key});
 
   @override
+  MyIconsRowState createState() => MyIconsRowState();
+}
+
+class MyIconsRowState extends State<MyIconsRow> {
+  final Random random = Random();
+
+  Color getRandomColor() {
+    return Color.fromRGBO(
+      random.nextInt(256), // Red
+      random.nextInt(256), // Green
+      random.nextInt(256), // Blue
+      1.0, // Opacity
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        Icon(Icons.favorite, color: Colors.red, size: 48),
+        IconButton(
+          onPressed: () {
+            setState(() {});
+          },
+          icon: Icon(Icons.favorite, color: getRandomColor(), size: 48),
+        ),
         Icon(Icons.bookmark, color: Colors.green, size: 48),
         Icon(Icons.star, color: Colors.blue, size: 48),
       ],
